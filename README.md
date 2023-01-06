320984059d7a4dee69ba57427f6e7e32681cf9db
mpiron <murielle.piron@gmail.com>
2023-01-07 00:44:27 +0100

    Etape7 Jouer une carte

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Uno5TT
 Création d'un jeu d'Uno en PHP avec les élèves de 5TT 
-
-Les étapes à réaliser: 
-1) Faire disparaitre petit à petit les étapes 1 à 8 pour obtenir un jeu jouable. Les étapes 1 à 8 étant la découverte du jeu. 
-
-Il faudra également créer un fonction  qui teste si la carte jouée peut être jouée. 
-1) Gérer ensuite les cartes spécialise
-2) Rendre le joueur 2 autonome.
+étape 1. Images affichées à partir d'un tableau associatif..
+étape 2. Mélange des  images grâce à la fonction php (shuffle)
+étape 3. distribution de 7 cartes. 
+            Affiche le deck mélangé mais aussi
+            La main du 1er joueur
+            La main du 2e joueur
+            La carte "défausse" sur laquelle nous allons jouer
+            les cartes qu'il reste dans le deck.  
+étape 4. Utilisation de la variable Session  (sans doute inutile) Contient également l'étape 5
+étape 5-6. Distribuer les cartes (les fonctions!!) 
+étape 7. rendre les carte cliquables
--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <!-- index.php -->
 <!-- inclusion des variables et fonctions -->
 <?php
-include_once('variables.php');
+session_start(); //donne acces à la supervariable $_SESSION qui persiste entre 2 requêtes PHP
+include('variables.php');
+include_once('functions.php');
+
+if (isset($_POST['click'])) {
+    if ($_POST['click'] == "j1") {
+        piocher('j1');
+    }
+    if ($_POST['click'] == "j2") {
+        piocher('j2');
+    }
+}
+if (isset($_GET['action'])) {
+    joue($_GET['joueur'], $_GET['numero']);
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,36 +25,96 @@ include_once('variables.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jeu UNO - Page d'accueil</title>
+    <title>Jeu UNO - Etape7</title>
     <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" type="image/jpg" href="cartes/uno.png" />
 </head>
 
 <body>
-
     <!-- inclusion de l'entête du site -->
     <?php include_once('header.php'); ?>
     <div id="container">
-        <h1>Présentation des cartes</h1>
+        <h1>Etape 7<br> Rendre les cartes cliquables </h1>
+        les étapes:
+        <ol>
+            <li>Le deck mélangé : $deckDepart <?php taille($deckDepart); ?> </li>
 
-        <?php foreach ($cartes as $carte) : ?>
-            <img class="carte" src="cartes/<?php echo $carte['image']; ?>" alt="<?php echo $carte['nom']; ?>">
-        <?php endforeach ?>
 
-        <form action="etape2.php" method="post">
-            <button type="submit">Passer à l'étape 2</button>
-        </form>
-        <form action="etape4.php" method="post">
-            <button type="submit">Accès direct à l'étape 4</button>
-        </form>
-        <form action="jeu.php" method="post">
-            <button type="submit">Jouer</button>
-        </form>
-    </div>
-    <!-- inclusion du bas de page du site -->
-    <?php include_once('footer.php'); ?>
+            <?php foreach ($deckDepart as $carte) : ?>
+                <img class="cartept" src="cartes/<?php echo $carte['image']; ?>" alt="<?php echo $carte['nom']; ?>">
+            <?php endforeach ?>
 
+
+            <li>Affichage des deux mains </li>
+            <?php if (!isset($_SESSION['mainJoueur1']))
+            // distribution des cartes
+            {
+                $mainJoueur1 = [];
+                $mainJoueur2 = [];
+                $defausse = [];
+                for ($i = 0; $i < 7; $i++) {
+                    $mainJoueur1 = array_merge($mainJoueur1, array_splice($deckDepart, 0, 1));
+                    $mainJoueur2 = array_merge($mainJoueur2, array_splice($deckDepart, 0, 1));
+                }
+                $defausse = array_merge($defausse, array_splice($deckDepart, 0, 1));
+                $_SESSION['mainJoueur1'] = $mainJoueur1;
+                $_SESSION['mainJoueur2'] = $mainJoueur2;
+                $_SESSION['defausse'] = $defausse;
+                $_SESSION['pioche'] = $deckDepart;
+            }
+            ?>
+            <ul>
+                <li>Main joueur 1 = <?php taille($_SESSION['mainJoueur1']); ?>
+                    <form action="index.php" method="post">
+                        <input type="hidden" name="click" value="j1" />
+                        <button type="submit">Joueur1: piocher des cartes</button>
+                    </form>
+                </li>
+                <?php afficherMain($_SESSION['mainJoueur1'], 'gd', 'mainJoueur1') ?>
+                <li>Main joueur 2 = <?php taille($_SESSION['mainJoueur2']); ?>
+                    <form action="index.php" method="post">
+                        <input type="hidden" name="click" value="j2" />
+                        <button type="submit">Joueur2: piocher des cartes</button>
+                    </form>
+                </li>
+                <?php afficherMain($_SESSION['mainJoueur2'], 'gd', 'mainJoueur2') ?>
+                <li>Défausse</li>
+                <?php foreach ($_SESSION['defausse'] as $carte) : ?>
+                    <img class="cartegd" src="cartes/<?php echo $carte['image']; ?>" alt="<?php echo $carte['nom']; ?>">
+                <?php endforeach ?>
+            </ul>
+            <li>Pioche restante = $_SESSION['pioche'] <?php taille($_SESSION['pioche']); ?></li>
+            <?php afficherCartes($_SESSION['pioche'], 'pt') ?>
+
+
+            <h2>
+                <a href="https://www.delftstack.com/fr/howto/php/onclick-php/#utilisez-la-m%25C3%25A9thode-get-et-la-fonction-isset-pour-ex%25C3%25A9cuter-une-fonction-php-%25C3%25A0-partir-dun-lien">Source pour joueur</a>
+            </h2>
+            <a href='index.php?joue=joueur2'>Execute PHP Function</a>
+
+</html>
+
+<?php if (isset($_GET['joue'])) {
+    echo ('<h1>Bravo</h1>');
+} ?>
+
+
+
+</ol>
+
+<form action="etape8.php" method="post">
+    <button type="submit">Aller à l'étape 8: rangement</button>
+</form>
+
+<p> Aide à l'étape ( <a target="_blank" href="https://openclassrooms.com/fr/courses/918836-concevez-votre-site-web-avec-php-et-mysql/4239476-conservez-des-donnees-grace-aux-sessions-et-aux-cookies">gérer
+        les sessions</a> )</p>
+
+
+</div>
+<!-- inclusion du bas de page du site -->
+<?php include_once('footer.php'); ?>
 
 </body>
 
 </html>
+<!-- Lien peut-être utile https://openclassrooms.com/forum/sujet/transmettre-un-array-via-formulaire-89193 -->

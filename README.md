2aa0ffa83761d9e2068a90283b51f07b6ad2361d
mpiron <murielle.piron@gmail.com>
2023-01-06 22:01:03 +0100

    Etape2 on mélange

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Uno5TT
 Création d'un jeu d'Uno en PHP avec les élèves de 5TT 
+Etape 1: ...
+Etape 2: on mélange les cartes!  
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<!-- index.php -->
-<!-- inclusion des variables et fonctions -->
-<?php
-include_once('variables.php');
-?>
+<!-- etape2.php -->
+<!-- inclusion des variables -->
+        <?php
+            include_once('variables.php');
+        ?>
 
 <!DOCTYPE html>
 <html>
@@ -11,7 +11,7 @@ include_once('variables.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jeu UNO - Page d'accueil</title>
+    <title>Jeu UNO - Etape 2</title>
     <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" type="image/jpg" href="cartes/uno.png" />
 </head>
@@ -20,27 +20,19 @@ include_once('variables.php');
 
     <!-- inclusion de l'entête du site -->
     <?php include_once('header.php'); ?>
-    <div id="container">
-        <h1>Présentation des cartes</h1>
 
-        <?php foreach ($cartes as $carte) : ?>
-            <img class="carte" src="cartes/<?php echo $carte['image']; ?>" alt="<?php echo $carte['nom']; ?>">
+    <div id="container">
+        <h1>Cartes mélangées</h1>
+        <?php
+            shuffle($cartes) ;
+            ?>
+        <?php foreach($cartes as $carte) : ?>
+        <img class="carte" src="cartes/<?php echo $carte['image']; ?>" alt="<?php echo $carte['nom']; ?>">
         <?php endforeach ?>
 
-        <form action="etape2.php" method="post">
-            <button type="submit">Passer à l'étape 2</button>
-        </form>
-        <form action="etape4.php" method="post">
-            <button type="submit">Accès direct à l'étape 4</button>
-        </form>
-        <form action="jeu.php" method="post">
-            <button type="submit">Jouer</button>
-        </form>
     </div>
     <!-- inclusion du bas de page du site -->
     <?php include_once('footer.php'); ?>
-
-
 </body>
 
 </html>

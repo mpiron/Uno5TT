7a072f3290ade5d9ee6ea86df0cfef9467ba91a0
mpiron <murielle.piron@gmail.com>
2023-01-06 22:16:41 +0100

    Etape3  distribution des cartes

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Uno5TT
 Création d'un jeu d'Uno en PHP avec les élèves de 5TT 
+étape 1. Images affichées à partir d'un tableau associatif..
+étape 2. Mélange des  images grâce à la fonction php (shuffle)
+étape 3. distribution de 7 cartes. 
+            Affiche le deck mélangé mais aussi
+            La main du 1er joueur
+            La main du 2e joueur
+            La carte "défausse" sur laquelle nous allons jouer
+            les cartes qu'il reste dans le deck.  
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-<!-- index.php -->
-<!-- inclusion des variables et fonctions -->
-<?php
-include_once('variables.php');
-?>
-
+<!-- etape3.php -->
+<!-- inclusion des variables -->
+        <?php
+            include_once('variables.php');
+        ?>
+        
 <!DOCTYPE html>
 <html>
 
@@ -11,7 +11,7 @@ include_once('variables.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jeu UNO - Page d'accueil</title>
+    <title>Jeu UNO - Etape3</title>
     <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" type="image/jpg" href="cartes/uno.png" />
 </head>
@@ -20,27 +20,54 @@ include_once('variables.php');
 
     <!-- inclusion de l'entête du site -->
     <?php include_once('header.php'); ?>
+
     <div id="container">
-        <h1>Présentation des cartes</h1>
+        <h1>Affichage mains</h1>
+        les étapes:
+        <ol>
+            <li>Le deck mélangé:</li>
+            <?php  
+            shuffle($cartes) ;?>
 
-        <?php foreach ($cartes as $carte) : ?>
-            <img class="carte" src="cartes/<?php echo $carte['image']; ?>" alt="<?php echo $carte['nom']; ?>">
-        <?php endforeach ?>
+            <?php foreach($cartes as $carte) : ?>
+            <img class="cartept" src="cartes/<?php echo $carte['image']; ?>" alt="<?php echo $carte['nom']; ?>">
+            <?php endforeach ?>
 
-        <form action="etape2.php" method="post">
-            <button type="submit">Passer à l'étape 2</button>
-        </form>
-        <form action="etape4.php" method="post">
-            <button type="submit">Accès direct à l'étape 4</button>
-        </form>
-        <form action="jeu.php" method="post">
-            <button type="submit">Jouer</button>
-        </form>
+
+            <li>Distribuer les deux mains</li>
+            <?php  
+         
+            
+            $mainJoueur1=[]; $mainJoueur2=[] ;$defausse=[];
+            for ($i=0;$i<7;$i++){ $mainJoueur1=array_merge($mainJoueur1,array_splice($cartes,0,1));
+                $mainJoueur2=array_merge($mainJoueur2,array_splice($cartes,0,1));}
+                $defausse=array_merge($defausse,array_splice($cartes,0,1)); ?>
+            
+            <ul>
+                <li>Main joueur 1</li>
+                <?php foreach($mainJoueur1 as $carte) : ?>
+                <img class="cartegd" src="cartes/<?php echo $carte['image']; ?>" alt="<?php echo $carte['nom']; ?>">
+                <?php endforeach ?>
+                <li>Main joueur 2</li>
+                <?php foreach($mainJoueur2 as $carte) : ?>
+                <img class="cartegd" src="cartes/<?php echo $carte['image']; ?>" alt="<?php echo $carte['nom']; ?>">
+                <?php endforeach ?>
+                <li>Défausse</li>
+                <?php foreach($defausse as $carte) : ?>
+                <img class="cartegd" src="cartes/<?php echo $carte['image']; ?>" alt="<?php echo $carte['nom']; ?>">
+                <?php endforeach ?>
+            </ul>
+            <li>Pioche restante</li>
+            <?php foreach($cartes as $carte) : ?>
+            <img class="cartept" src="cartes/<?php echo $carte['image']; ?>" alt="<?php echo $carte['nom']; ?>">
+            <?php endforeach ?>
+
+        </ol>
+       
     </div>
     <!-- inclusion du bas de page du site -->
     <?php include_once('footer.php'); ?>
-
-
+    
 </body>
 
 </html>
